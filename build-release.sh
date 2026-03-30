89c3e77401793c59c07ace3952631c626ff0c676
mauritsdebruin <79079263+mauritsdebruin@users.noreply.github.com>
2026-03-30 10:21:23 +0200

    CLOUD-3617 Add Windows ARM64 release build

--- build-release.sh
+++ build-release.sh
@@ -12,12 +12,14 @@ LDFLAG="$LDFLAG -X $CMD_PKG.GitTag=$GIT_TAG"
 
 GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAG" -o build/linux-amd64/aws-keyhub 
 GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAG" -o build/windows-amd64/aws-keyhub.exe
-GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAG" -o build/darwin-amd64/aws-keyhub 
-GOOS=darwin GOARCH=arm64 go build -ldflags "$LDFLAG" -o build/darwin-arm64/aws-keyhub 
+GOOS=windows GOARCH=arm64 go build -ldflags "$LDFLAG" -o build/windows-arm64/aws-keyhub.exe
+GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAG" -o build/darwin-amd64/aws-keyhub
+GOOS=darwin GOARCH=arm64 go build -ldflags "$LDFLAG" -o build/darwin-arm64/aws-keyhub
 
 # Make release zips
 mkdir -p release/
 zip -j release/aws-keyhub-linux-amd64.zip build/linux-amd64/*
 zip -j release/aws-keyhub-windows-amd64.zip build/windows-amd64/*
+zip -j release/aws-keyhub-windows-arm64.zip build/windows-arm64/*
 zip -j release/aws-keyhub-darwin-arm64.zip build/darwin-arm64/*
 zip -j release/aws-keyhub-darwin-amd64.zip build/darwin-amd64/*
